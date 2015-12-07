4ec80d101012619e44a2675ffb63f29d675b197b
Hardik Patel <hardik@circulumvite.com>
2015-12-07 10:28:26 -0500

    adding git config

---
+++ setupgit.sh
@@ -0,0 +1,15 @@
+git config --global core.editor "vi"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+
+git config --global alias.st status
+git config --global alias.df diff
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+
+git config --global push.default upstream
+
+git config --global user.name "Hardik Patel"
+git config --global user.email "hardik@circulumvite.com"
