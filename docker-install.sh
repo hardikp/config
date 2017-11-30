b2693d6242a438345225d8f024f51b8ce7a07687
Hardik Patel <hardikp12@gmail.com>
2017-11-29 22:45:12 -0500

    docker

---
+++ docker-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installation instructions for docker-ce in ubuntu 16.04+
+# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
+
+sudo apt remove docker docker-engine docker.io
+sudo apt update
+sudo apt install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker `whoami`
+echo "Please logout/login for the usermod changes to take effect!"
